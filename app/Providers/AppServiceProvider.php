f12707e435134d11dedefa1206199a5f1235bbd4
Mori Mn <moradi.n1984@gmail.com>
2026-02-23 19:26:22 +0100

    clean-code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
 use Laravel\Reverb\Events\MessageReceived;
-use App\Listeners\HandleWsMessageReceived;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // Event::listen(
-        //     MessageReceived::class,
-        //     HandleWsMessageReceived::class,
-        // );
+     
     }
 }
